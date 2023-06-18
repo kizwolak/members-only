1b44de0d502912cda77102359d4d858ce25552fb
kizwolak <ki.zwolak@gmail.com>
2023-06-18 04:56:06 -0300

    Add sign-up form

---
+++ controllers/AuthenticationController.js
@@ -0,0 +1,67 @@
+const mongoose = require("mongoose");
+const { body, validationResult } = require("express-validator");
+const asyncHandler = require("express-async-handler");
+const User = require("../models/user");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
+
+exports.signup = [
+  body("first_name", "First name must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("last_name", "Last name must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("username", "Username must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("password", "Password must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape()
+    .custom((value, { req, loc, path }) => {
+      if (value !== req.body.password_confirm) {
+        throw new Error("Passwords don't match");
+      } else {
+        return value;
+      }
+    }),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    console.log("first name: " + req.body.first_name);
+
+    if (!errors.isEmpty()) {
+      res.render("sign-up", {
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        username: req.body.username,
+        errors: errors.array(),
+      });
+      return;
+    }
+
+    try {
+      bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+        if (err) {
+          return next(err);
+        } else {
+          const user = new User({
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            username: req.body.username,
+            password: hashedPassword,
+          });
+          const result = await user.save();
+          res.redirect("/");
+        }
+      });
+    } catch (err) {
+      return next(err);
+    }
+  }),
+];
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const router = express.Router();
+const AuthenticationController = require("../controllers/AuthenticationController");
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Members Only" });
 });
+
+router.get("/sign-up", function (req, res, next) {
+  res.render("sign-up");
+});
+
+router.post("/sign-up", AuthenticationController.signup);
 
 module.exports = router;
